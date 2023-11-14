02c44812fc9544c953a0b2ceb567c3ebf3497179
dheeraj <drjsde@gmail.com>
2023-11-15 00:40:57 +0530

    Add CharacterListingPage & CharacterDetails component

---
+++ src/components/CharacterDetails.jsx
@@ -0,0 +1,50 @@
+function CharacterDetails() {
+  return (
+    <table className="detail-table">
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>name</td>
+          </tr>
+          <tr>
+            <td>WikiURL</td>
+            <td>WikiURL</td>
+          </tr>
+          <tr>
+            <td>Race</td>
+            <td>Race</td>
+          </tr>
+          <tr>
+            <td>Height</td>
+            <td>Height</td>
+          </tr>
+          <tr>
+            <td>Hair</td>
+            <td>Hair</td>
+          </tr>
+          <tr>
+            <td>Realm</td>
+            <td>Realm</td>
+          </tr>
+          <tr>
+            <td>Birth</td>
+            <td>Birth</td>
+          </tr>
+          <tr>
+            <td>Gender</td>
+            <td>Gender</td>
+          </tr>
+          <tr>
+            <td>Spouse</td>
+            <td>Spouse</td>
+          </tr>
+          <tr>
+            <td>Death</td>
+            <td>Death</td>
+          </tr>
+        </tbody>
+    </table>
+  );
+}
+
+export default CharacterDetails;
--- src/pages/character-detail-page/index.jsx
+++ src/pages/character-detail-page/index.jsx
@@ -1,8 +1,19 @@
-import "../../styles/CharacterDetailPage.css"
+import CharacterDetailsPreview from "../../components/CharacterDetails";
+import Header from "../../components/Header";
+import "../../styles/CharacterDetailPage.css";
 function CharacterDetailPage() {
   return (
-    <div>CharacterListingPage</div>
-  )
+    <section className="detail-section">
+      <Header />
+      <main className="table-container">
+        <CharacterDetailsPreview />
+      </main>
+      <span></span>
+      <div className="close-btn-div">
+        <div className="close-btn">Close</div>
+      </div>
+    </section>
+  );
 }
 
-export default CharacterDetailPage
+export default CharacterDetailPage;
